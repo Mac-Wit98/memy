cf27ea59f9ba2b94b4fecb12613cb61fddc715aa
Maciej Witos <witosek98@wp.pl>
2018-07-31 10:44:26 +0200

    Added Controller

---
+++ src/main/java/pl/akademiakodu/memy/controller/MemController.java
@@ -0,0 +1,12 @@
+package pl.akademiakodu.memy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemController {
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+}
