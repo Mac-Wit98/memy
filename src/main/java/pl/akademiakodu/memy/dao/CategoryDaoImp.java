06fc3088b8ce8a76611c56a618b93aef829c10a4
lukaszgeb123 <lukaszgeb123@gmail.com>
2018-07-31 16:49:16 +0200

    heroku

---
+++ src/main/java/pl/akademiakodu/memy/dao/CategoryDaoImp.java
@@ -0,0 +1,14 @@
+package pl.akademiakodu.memy.dao;
+
+import pl.akademiakodu.memy.model.Category;
+
+import java.util.List;
+
+public class CategoryDaoImp implements CategoryDao {
+
+
+    @Override
+    public List<Category> findAll() {
+        return null;
+    }
+}
